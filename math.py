8e8322eb706c22e56f1407fcadb0570034b57676
MayankMishra3 <86823399+MayankMishra3@users.noreply.github.com>
2021-07-02 14:38:31 +0530

    next update math.py

--- math.py
+++ math.py
@@ -4,10 +4,10 @@ def add(x,y):
   
 def subtract(x,y):
     return x-y     #on Bug456
-  
+ #multiply implemented
 def multiply(x,y):
     return x*y       #on master
-  
+  #division implemented
 def divide(x,y):
     if y == 0:
         return operation_invalid
